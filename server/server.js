8b4ab4a543183141eaf414a76e88c629551f15da
sasham43 <sashasemail@gmail.com>
2016-08-05 13:48:29 -0500

    add to args

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ app.get('/', function(res, req){
 io.on('connection', function(socket){
   socket.on('play', function(data){
     console.log('play');
-    var args = [];
+    var args = ['omxplayer'];
     var options = { cwd: undefined, env: process.env };
     console.log('process.env.PATH:', process.env.PATH );
-    var child = cp.spawn('sudo /usr/bin/omxcdplayer', args, options).on('error', function( err ){ throw err });
+    var child = cp.spawn('~/', args, options).on('error', function( err ){ throw err });
     child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
     });
