c0ebd2a6fe50ec9e776934e7d6c72287f56ec393
sasham43 <sashasemail@gmail.com>
2016-08-05 12:10:08 -0500

    Add commands

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -1,5 +1,22 @@
 $(function(){
   var socket = io();
 
-  console.log('loaded.', socket);
+  socket.on('socket connected', function(){
+    console.log('socket connected');
+  });
+
+  $(".play").on("click", function(){
+    socket.emit('play');
+  });
+  $(".prev").on("click", function(){
+    socket.emit('prev');
+  });
+  $(".next").on("click", function(){
+    socket.emit('next');
+  });
+  $(".eject").on("click", function(){
+    socket.emit('eject');
+  });
+
+  console.log('loaded.');
 })
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cp = require('child_process');
 
 var app = express();
 var server = require('http').Server(app);
@@ -7,8 +8,41 @@ var port = 3001;
 
 app.use(express.static('server/public'));
 
-server.listen(port);
-
 app.get('/', function(res, req){
   res.sendFile('./public/index.html');
 });
+
+io.on('connection', function(socket){
+  socket.on('play', function(data){
+    console.log('play');
+    cp.exec('omxcdplayer', cpLog);
+  });
+
+  socket.on('eject', function(data){
+    console.log('eject');
+    cp.exec('eject /dev/cdrom', cpLog);
+  });
+  socket.on('prev', function(data){
+    console.log('prev');
+    cp.exec('<', cpLog);
+  });
+  socket.on('next', function(data){
+    console.log('next');
+    cp.exec('>', cpLog);
+  });
+
+  console.log('socket connected.');
+  socket.emit('socket connected');
+});
+
+server.listen(port);
+
+cpLog = (error, stdout, stderr) => {
+  if(error){
+    console.error(`error: ${error}`);
+    return;
+  }
+
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+}
